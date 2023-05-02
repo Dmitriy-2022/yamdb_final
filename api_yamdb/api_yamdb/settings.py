4be2b7ea9762a0439ba6a2d44361c0a874a200a5
Dmitriy-2022 <mitya.podmorin99@yandex.ru>
2023-05-02 11:31:32 +0300

    db_v1.16

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -17,7 +17,10 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '51.250.97.100']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
